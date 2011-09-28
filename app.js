2723b17035720ccc9b9a1da92255aa489faa7bd7
peterhimschoot <peter.himschoot@gmail.com>
2011-09-28 11:56:58 +0000

    Added app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = module.exports = express.createServer();
+
+//configure express
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+//start the http server
+app.listen(process.env.C9_PORT);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
