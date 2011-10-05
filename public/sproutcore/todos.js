008c4d6fca8dc493c4722b07638731a50c07255f
Peter Himschoot <peter.himschoot@gmail.com>
2011-10-05 20:42:30 +0000

    added sproutcore stuff

--- public/sproutcore/todos.js
+++ public/sproutcore/todos.js
@@ -16,7 +16,29 @@ Todos.todosController = SC.ArrayProxy.create({
   createTodo: function(title) {
     var todo = Todos.Todo.create({ title: title });
     this.pushObject(todo);
-  }
+  },
+  
+  remaining: function() {
+    return this.filterProperty('isDone', false).get('length');
+  }.property('@each.isDone'),
+  
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }
+  }.property('@each.isDone')
+});
+
+Todos.StatsView = SC.View.extend({
+  remainingBinding: 'Todos.todosController.remaining',
+ 
+  remainingString: function() {
+    var remaining = this.get('remaining');
+    return remaining + (remaining === 1 ? " item" : " items");
+  }.property('remaining')
 });
 
 Todos.CreateTodoView = SC.TextField.extend({
